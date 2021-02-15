6d255e94f270c27665f19252b3ffd82cf11a6930
pablocastillo123 <pablocastillo325@gmail.com>
2021-02-15 00:32:21 -0400

    creacion de servicio auth

--- Tinder/src/app/app.module.ts
+++ Tinder/src/app/app.module.ts
@@ -7,14 +7,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { environment } from '../environments/environment'
-import { AngularFireModule  } from 'angularfire2'
-import {  AngularFirestoreModule } from 'angularfire2/firestore'
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFirestoreModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
