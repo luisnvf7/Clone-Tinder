8cc38148698488140c8c2d04a244cc85883444f1
luisnvf7 <luisnazario2310@gmail.com>
2021-02-24 00:02:07 -0400

    Service Toast

---
+++ Tinder/src/app/services/toast.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import { ToastController } from '@ionic/angular';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+
+  constructor(public toastController: ToastController) { }
+
+  public async presentToast(message: string) : Promise<void> {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+}
