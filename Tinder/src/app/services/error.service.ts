20457dd9dc09b4629734c2c68cb5fd16dd84197d
luisnvf7 <luisnazario2310@gmail.com>
2021-02-24 00:41:58 -0400

    Servicios

--- Tinder/src/app/services/error.service.ts
+++ Tinder/src/app/services/error.service.ts
@@ -8,8 +8,20 @@ export class ErrorService {
 
   constructor(private _toastService : ToastService) { }
 
-  public getErrors(error: string ) : void {
-    
+  public getErrors(control: any) : void {
+    let array = []
+      for (const prop in control) {
+        for (const prop1 in control[prop].errors) {
+          switch(prop1) {
+            case "minlength": 
+              array.push(`Por favor ingrese mas datos en ${prop}`)
+            break;
+            case "required":
+              array.push(`${ prop[0].toUpperCase() + prop.substring(1)} es requerido`)
+          }
+        }
+      }
+      this._toastService.presentToast(array.join("\n"), "top", "light")
   }
 
 
--- Tinder/src/app/services/toast.service.ts
+++ Tinder/src/app/services/toast.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
-
 import { ToastController } from '@ionic/angular';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToastService {
 
-  constructor(public toastController: ToastController) { }
+  constructor(private _toastController: ToastController) { }
 
-  public async presentToast(message: string) : Promise<void> {
-    const toast = await this.toastController.create({
+  public async presentToast(message: string, position: "top" | "bottom" | "middle", color : any) : Promise<void> {
+    const toast = await this._toastController.create({
       message,
-      duration: 2000
+      duration: 2000,
+      position,
+      color
     });
     toast.present();
   }
