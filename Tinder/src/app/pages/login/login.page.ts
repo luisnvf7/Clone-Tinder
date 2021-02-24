bc5ed92e464f39f58334d056b4e6b7e9ee6ebecc
luisnvf7 <luisnazario2310@gmail.com>
2021-02-24 01:15:43 -0400

    Login adelantos

--- Tinder/src/app/pages/login/login.page.ts
+++ Tinder/src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
+import { ErrorService } from 'src/app/services/error.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private _fb: FormBuilder, private _errorService : ErrorService) { }
+
+  public loginForm = this._fb.group({
+    username: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]],
+    password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]]
+  })
 
   ngOnInit() {
   }
+  
+  public onLogin() : void {
+    // this._toastService.presentToast("Supuesto Error")
+    if (this.loginForm.status === "INVALID") {
+      this._errorService.getErrors(this.loginForm.controls)
+    } else {
+      console.log("VER ", this.loginForm.get('username').value)
+      console.log("PASSWORD", this.loginForm.get('password').value)
+      console.log("LOGIN", this.loginForm)
+      //Peticion a firebase 
+  
+      //Termina de hacer peticion, vaciamos valores.
+      // this.loginForm.patchValue({
+      //   username: '',
+      //   password: ''
+      // })
+      
+      //Ir a home
+    }
 
+  }
 }
