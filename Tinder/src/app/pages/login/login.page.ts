a4b443601ae80277df17b5088dd9f168a62e44ab
luisnvf7 <luisnazario2310@gmail.com>
2021-02-24 23:44:39 -0400

    Cambios

--- Tinder/src/app/pages/login/login.page.ts
+++ Tinder/src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 import { ErrorService } from 'src/app/services/error.service';
 
 @Component({
@@ -9,7 +10,7 @@ import { ErrorService } from 'src/app/services/error.service';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private _fb: FormBuilder, private _errorService : ErrorService) { }
+  constructor(private _fb: FormBuilder, private _errorService : ErrorService, private _authService : AuthService ) { }
 
   public loginForm = this._fb.group({
     username: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]],
@@ -24,9 +25,10 @@ export class LoginPage implements OnInit {
     if (this.loginForm.status === "INVALID") {
       this._errorService.getErrors(this.loginForm.controls)
     } else {
-      console.log("VER ", this.loginForm.get('username').value)
-      console.log("PASSWORD", this.loginForm.get('password').value)
-      console.log("LOGIN", this.loginForm)
+      // console.log("VER ", this.loginForm.get('username').value)
+      // console.log("PASSWORD", this.loginForm.get('password').value)
+      // console.log("LOGIN", this.loginForm)
+      this._authService.login(this.loginForm.get('username').value, this.loginForm.get('password').value)
       //Peticion a firebase 
   
       //Termina de hacer peticion, vaciamos valores.
--- Tinder/src/app/services/auth.service.ts
+++ Tinder/src/app/services/auth.service.ts
@@ -4,14 +4,22 @@ import {UserInfoInterface} from "../models/userInfoInterface"
 
 import { AngularFireAuth } from "@angular/fire/auth"
 
+import { AngularFirestore, AngularFirestoreCollection  } from "@angular/fire/firestore"
+
+import { Observable } from 'rxjs';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
   private _userInformation : UserInfoInterface
+  private _usersCollection : AngularFirestoreCollection
 
-  constructor(private _auth : AngularFireAuth) {}
+  constructor(private _auth : AngularFireAuth , private _store : AngularFirestore) {
+    this._usersCollection = this._store.collection('users')
+  }
 
   public get userInformation () {
     return this._userInformation
@@ -25,18 +33,36 @@ export class AuthService {
     
   }
 
-  public login () : void {
-
+  public async login (username: string, password: string) : Promise<void> {
+      console.log("USERNAME", username)
+      console.log("PASSWORD", password)
+      try {
+        const result : any = await this._auth.signInWithEmailAndPassword(username + "@gmail.com", password)
+        console.log("RESULT", result)
+        this._getUserById(result.user.uid).subscribe(v => console.log("VALOR", v))
+      } catch(e){
+        console.log("NO TE PUDISTE LOGEAR")
+      }
   }
 
-  public register () : void {
-    
+  public register () : void {}
+
+  public async logOut() : Promise<void> {
+    console.log("CERRANDO SESION...")
+  }
+
+  private _getUserById (id : string) : Observable<any> {
+    return this._usersCollection.doc(id).valueChanges()
   }
 
 }
 
 /* 
   {
+    credentialsInfo: {
+      username: "luisnvf7",
+      password: "12345678"
+    },
     gender: {
       id: 1,
       name: "Hombre",
