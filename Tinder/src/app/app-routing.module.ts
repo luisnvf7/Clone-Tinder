c8edf1fd9ed0c2b0aa9d74a097d1c6b3384e99e6
pablocastillo123 <pablocastillo325@gmail.com>
2021-03-01 23:14:55 -0400

    page profile. modificacion credentials-info y personal-information

--- Tinder/src/app/app-routing.module.ts
+++ Tinder/src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  },  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
   },
+
 ];
 
 @NgModule({
